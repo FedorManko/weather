6ad4aa9f93e59a5dcf1c8c55743fab9b425f101f
MankoF <f.manko@andersenlab.com>
2023-12-12 09:00:49 +0300

    add roles check

---
+++ src/main/java/com/andersen/weather/config/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.andersen.weather.config.security;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  private final JwtAuthConverter jwtAuthConverter;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(request -> request
+            .requestMatchers("/", "/swagger-ui/**", "/api-docs/**", "/actuator/**")
+            .permitAll()
+            .requestMatchers(GET, "/weather").permitAll()
+            .anyRequest().authenticated());
+
+    http.oauth2ResourceServer(
+        auth -> auth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)));
+
+    http.sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
+
+    return http.build();
+  }
+
+}
--- src/main/java/com/andersen/weather/controller/WeatherController.java
+++ src/main/java/com/andersen/weather/controller/WeatherController.java
@@ -14,6 +14,7 @@ import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,7 @@ public class WeatherController {
   private final WeatherService weatherService;
 
   @PostMapping
+  @PreAuthorize("hasRole('EDITOR')")
   @BadRequestResponse
   @ApiResponse(responseCode = "201")
   @Operation(summary = "Creates new weather record")
